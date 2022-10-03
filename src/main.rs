d8139c436b64d405170fcc264f31d3cd5fbf72f4
Brian Bostwick <bostwick.l.brian@gmail.com>
2022-10-03 17:02:36 +0100

    adding collisions

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,23 @@
 //! Single particle in a cross beam optical dipole trap
 extern crate atomecs as lib;
 extern crate nalgebra;
-use lib::atom::{self, Position, Velocity};
-use lib::atom::Atom;
+use lib::atom::{self, Atom, Force, Mass, Position, Velocity};
 use lib::dipole::{self, DipolePlugin};
 use lib::integrator::Timestep;
 use lib::laser::{self, LaserPlugin};
 use lib::laser::gaussian::GaussianBeam;
-use lib::output::file::{FileOutputPlugin};
-use lib::output::file::{Text, XYZ};
+use lib::laser::intensity::{LaserIntensitySamplers};
+use lib::output::file::{FileOutputPlugin, Text, XYZ};
 use lib::simulation::SimulationBuilder;
 use nalgebra::Vector3;
 use specs::prelude::*;
 use std::time::Instant;
+use rand_distr::{Distribution, Normal};
+use lib::initiate::NewlyCreated;
 use std::fs::File;
 use std::io::{Error, Write};
-use lib::collisions::CollisionPlugin;
-use lib::collisions::{ApplyCollisionsOption, CollisionParameters, CollisionsTracker};
+use lib::collisions::{CollisionPlugin, ApplyCollisionsOption, CollisionParameters, CollisionsTracker};
+// use lib::atom_sources::gaussian::{GaussianCreateAtomsSystem};
 
 const BEAM_NUMBER: usize = 2;
 
@@ -25,30 +26,21 @@ fn main() {
 
     // Configure simulation output.
     let mut sim_builder = SimulationBuilder::default();
+
     sim_builder.add_plugin(LaserPlugin::<{BEAM_NUMBER}>);
     sim_builder.add_plugin(DipolePlugin::<{BEAM_NUMBER}>);
-    sim_builder.add_plugin(CollisionPlugin);
+
+    sim_builder.world.register::<NewlyCreated>();
     sim_builder.add_end_frame_systems();
-    sim_builder.add_plugin(FileOutputPlugin::<Position, Text, Atom>::new("pos.txt".to_string(), 1));
-    sim_builder.add_plugin(FileOutputPlugin::<Velocity, Text, Atom>::new("vel.txt".to_string(), 1));
-    sim_builder.add_plugin(FileOutputPlugin::<Position, XYZ, Atom>::new("position.xyz".to_string(), 1));
+    sim_builder.add_plugin(CollisionPlugin);
+
+    sim_builder.add_plugin(FileOutputPlugin::<Position, Text, Atom>::new("D:/data_1/pos.txt".to_string(), 1));
+    sim_builder.add_plugin(FileOutputPlugin::<Velocity, Text, Atom>::new("D:/data_1/vel.txt".to_string(), 1));
+    sim_builder.add_plugin(FileOutputPlugin::<Position, XYZ, Atom>::new("D:/data_1/position.xyz".to_string(), 1));
+    sim_builder.add_plugin(FileOutputPlugin::<LaserIntensitySamplers<{BEAM_NUMBER}>, Text, LaserIntensitySamplers<{BEAM_NUMBER}>>::new("intensity.txt".to_string(), 1));
+
     let mut sim = sim_builder.build();
 
-    sim.world.insert(ApplyCollisionsOption);
-    sim.world.insert(CollisionParameters {
-        macroparticle: 4e2,
-        box_number: 200, //Any number large enough to cover entire cloud with collision boxes. Overestimating box number will not affect performance.
-        box_width: 20e-6, //Too few particles per box will both underestimate collision rate and cause large statistical fluctuations.
-        //Boxes must also be smaller than typical length scale of density variations within the cloud, since the collisions model treats gas within a box as homogeneous.
-        sigma: 3.5e-16, //Approximate collisional cross section of Rb87
-        collision_limit: 10_000_000.0, //Maximum number of collisions that can be calculated in one frame.
-                                       //This avoids absurdly high collision numbers if many atoms are initialised with the same position, for example.
-    });
-    sim.world.insert(CollisionsTracker {
-        num_collisions: Vec::new(),
-        num_atoms: Vec::new(),
-        num_particles: Vec::new(),
-    });
 
     // Create dipole laser.
     let power = 7.0;
@@ -93,31 +85,63 @@ fn main() {
         })
         .build();
 
+
+    let p_dist = Normal::new(0.0, 50e-6).unwrap();
+    let v_dist = Normal::new(0.0, 0.004).unwrap(); // ~100nK
+
     // Create a single test atom
-    sim.world
-        .create_entity()
-        .with(atom::Mass { value: 87.0 })
-        .with(atom::Force::new())
-        .with(atom::Position {
-            pos: Vector3::new(1.0e-7, 1.0e-7, 1.0e-7),
-        })
-        .with(atom::Velocity {
-            vel: Vector3::new(0.0, 0.0, 0.0),
-        })
-        .with(dipole::Polarizability::calculate_for(
-            wavelength, 461e-9, 32.0e6,
-        ))
-        .with(atom::Atom)
-        .with(lib::initiate::NewlyCreated)
-        .build();
+    let atom_number = 25000;
+    for _i in 0..atom_number {
+        sim.world
+            .create_entity()
+            .with(Atom)
+            .with(Mass { value: 87.0 })
+            .with(Force::new())
+            .with(Position {
+                pos: Vector3::new(
+                    p_dist.sample(&mut rand::thread_rng()),
+                    p_dist.sample(&mut rand::thread_rng()),
+                    p_dist.sample(&mut rand::thread_rng()), //TOP traps have tighter confinement along quadrupole axis
+                ),
+            })
+            .with(atom::Velocity {
+                vel: Vector3::new(
+                    v_dist.sample(&mut rand::thread_rng()),
+                    v_dist.sample(&mut rand::thread_rng()),
+                    v_dist.sample(&mut rand::thread_rng()),
+                ),
+            })
+            .with(dipole::Polarizability::calculate_for(
+                wavelength, 461e-9, 32.0e6,
+            ))
+            .with(lib::initiate::NewlyCreated)
+            .build();
+    }
+
+    sim.world.insert(ApplyCollisionsOption);
+    sim.world.insert(CollisionParameters {
+        macroparticle: 4e2,
+        box_number: 200,  //Any number large enough to cover entire cloud with collision boxes. Overestimating box number will not affect performance.
+        box_width: 20e-6, //Too few particles per box will both underestimate collision rate and cause large statistical fluctuations.
+                          //Boxes must also be smaller than typical length scale of density variations within the cloud, since the collisions model treats gas within a box as homogeneous.
+        sigma: 3.5e-16,   //Approximate collisional cross section of Rb87
+        collision_limit: 10_000_000.0, //Maximum number of collisions that can be calculated in one frame.
+                                       //This avoids absurdly high collision numbers if many atoms are initialised with the same position, for example.
+    });
+    sim.world.insert(CollisionsTracker {
+        num_collisions: Vec::new(),
+        num_atoms: Vec::new(),
+        num_particles: Vec::new(),
+    });
 
     // Define timestep
-    sim.world.insert(Timestep { delta: 1.0e-7 });
+    sim.world.insert(Timestep { delta: 1.0e-6 });
+    //Timestep must also be much smaller than mean collision time
 
-    let mut filename = File::create("collisions.txt").expect("Cannot create file.");
+    let mut filename = File::create("D:/data_1/collisions.txt").expect("Cannot create file.");
 
     // Run the simulation for a number of steps.
-    for _i in 0..200_000 {
+    for _i in 0..10_000 {
         sim.step();
 
         if (_i > 0) && (_i % 50_i32 == 0) {
@@ -136,7 +160,7 @@ fn main() {
 }
 
 
-// // Write collision stats to file
+// Write collision stats to file
 
 fn write_collisions_tracker(
     filename: &mut File,
